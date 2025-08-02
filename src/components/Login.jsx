8924e94999214ed963a96e21590f8ac20ac5ffb7
Navaneeth PV <navaneethpv550@gmail.com>
2025-08-02 10:01:47 +0300

    `Refactor Login component styles and layout`

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,30 @@
 export default function Login() {
   return (
     <>
-      <div className="flex flex-col min-w-120 bg-amber-100">
-        <h1 className="text-5xl font-sans font-bold text-center mt-10 text-shadow-sm text-shadow-gray-500">
+      <div className="flex flex-col min-w-120 bg-white/30 backdrop-blur-xl rounded-xl">
+        <h1 className="text-5xl text-white font-serif font-bold text-center mt-10 text-shadow-sm text-shadow-gray-500">
           Login
         </h1>
         <div className="flex mt-1 justify-center items-center min-h-90">
           <form className="flex flex-col justify-center items-start mt-10">
-            <label className="p-2 ml-5 font-medium  text-shadow-zinc-600 text-shadow-sm">Username</label>
+            <label className="p-2 ml-5 text-lg font-medium text-white text-shadow-gray-500 text-shadow-sm">Username</label>
             <input
-              className="border-2 border-gray-500 px-10 py-1.5 ml-6 rounded-full placeholder-gray-500"
+              className="border-3 text-white font-mono border-white px-13 py-1.5 ml-6 rounded-full placeholder:text-white/60 placeholder:font-medium placeholder:font-sans"
               type="text"
               placeholder="Enter user name"
             />
-            <label className="p-2 ml-5 font-medium text-shadow-zinc-600 text-shadow-sm ">Password</label>
+            <label className="p-2 ml-5 text-lg font-medium text-white text-shadow-gray-500 text-shadow-sm mt-5">Password</label>
             <input
-              className="border-2 border-gray-500 px-10 py-1.5 ml-6 rounded-full placeholder-gray-500"
+              className="border-3 text-white font-mono border-white px-13 py-1.5 ml-6 rounded-full placeholder:text-white/60 placeholder:font-medium placeholder:font-sans"
               type="password"
               placeholder="Enter password"
             />
-            <button className="border-1 border-amber-200 from  px-30 py-1.5 ml-8 mt-10 rounded-full font-bold font-mono bg-gradient-to-r from-red-400 to-orange-300 shadow-sm shadow-black hover:cursor-pointer hover:from-red-500 hover:to bg-orange-500 hover:text-amber-100 transition-all duration-200 ease">
+            <button className="border-1 border-white/50 from text-white/90 px-30 py-1.5 ml-8 mt-10 rounded-full text-lg font-bold font-mono bg-gradient-to-r from-red-400 to-orange-300 shadow-sm shadow-black hover:cursor-pointer hover:from-red-500 hover:to bg-orange-500 hover:text-amber-100 transition-all duration-200 ease">
               Login
             </button>
           </form>
         </div>
-        <div className="flex justify-center gap-9">
+        <div className="flex justify-center gap-9 invert">
           <img
             src="public/Images/instagram.svg"
             alt="Instagram"
